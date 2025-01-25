133f155a205a1ee91ca9fef6cd45204985e32d0e
sadiquesq <sadiquesanu1@gmail.com>
2025-01-25 10:39:45 +0530

    Time: 9 ms (47.94%), Space: 48.5 MB (19.41%) - LeetHub

---
+++ 1168-duplicate-zeros/1168-duplicate-zeros.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public void DuplicateZeros(int[] arr) {
+          
+          for(int i=0;i<arr.Length-1;i++)
+          {
+            if(arr[i]==0)
+            {
+                for(int j=arr.Length-1;j>i;j--)
+                {
+                  arr[j]=arr[j-1];
+               
+                }
+                arr[i+1]=0;
+                i++;
+            }
+          }
+        
+    
+    }
+}

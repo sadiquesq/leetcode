6449348f04853dc6ce4e39abf497e1165ca8c952
sadiquesq <sadiquesanu1@gmail.com>
2025-02-20 09:43:00 +0530

    Time: 1 ms (38.46%), Space: 42.9 MB (34.62%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cs
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cs
@@ -1,14 +1,14 @@
 public class Solution {
     public string FindDifferentBinaryString(string[] nums) {
-        Array.Sort(nums);
+     
         int len=nums.Length;
         for(int i=0;i<nums.Length;i++)
         {
-            int a=Convert.ToInt32(nums[i],2);
-            if(a!=i)
-            {
-                return Convert.ToString(i,2).PadLeft(len,'0');
-            }
+          string a=Convert.ToString(i,2).PadLeft(len,'0');
+          if(!nums.Contains(a))
+          {
+            return a;
+          }
         }
         return Convert.ToString((nums.Length),2).PadLeft(len,'0');
     }
